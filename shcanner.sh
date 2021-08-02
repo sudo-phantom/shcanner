a6b042ef08dd0a3150340015e632ef69ce8be989
sudo-phantom <sudophantom@gmail.com>
2021-08-02 09:25:13 -0700

    Update shcanner.sh

--- shcanner.sh
+++ shcanner.sh
@@ -8,8 +8,7 @@ sudo apt-get update && sudo apt-get install -y nmap python python-pip;
 # run nmap scanns
 sudo nmap -sP -iL  scope.txt -oA livehost_icmp -vv;
 sudo nmap -sS -Pn -vv -p21,22,23,25,80,110,443,513,3389,6000,8080,8443,445,135,139 -iL scope.txt -oA livehost_standard;
-sudo nmap -n -Pn -iL livehost_icmp.gnmap -oG - | grep Up | cut -d ' ' -f 2 > up.txt
-
+cat livehost_icmp.gnmap | grep Up | cut -d " " -f 2 > up.txt | nmap -n -Pn -iL up.txt -vv -oG full_livehosts
 #traceroute hosts
-for /F ” ” %i in (‘type scope.txt’) do tracert -h 10 %i | grep -v “out” | gawk -F ” ” ” {print $8}” | sort;
+# no work ----    for /F ” ” %i in (‘type livehost_icmp.gnmap’) do tracert -h 10 %i | grep -v “out” | gawk -F ” ” ” {print $8}” | sort;
 #continue interface
