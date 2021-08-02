dd62a24801f2897e212818010615b5a6c74613d1
sudo-phantom <sudophantom@gmail.com>
2021-08-02 09:56:56 -0700

    Create shcanner.py

---
+++ shcanner.py
@@ -0,0 +1,23 @@
+import subprocess
+
+with open("up.txt", "r") as hostlist, open("results.txt", "wb") as output:
+    for host in hostlist:
+        host = host.strip()
+
+        print("Tracing", host)
+
+        trace = subprocess.Popen(["traceroute", "-w 50", host], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        while True:
+            hop = trace.stdout.readline()
+
+            if not hop: break
+
+            print( '-->', hop)
+
+            output.write(hop)
+
+        # When you pipe stdout, the doc recommends that you use .communicate()
+        # instead of wait()
+        # see: http://docs.python.org/2/library/subprocess.html#subprocess.Popen.wait
+        trace.communicate()
