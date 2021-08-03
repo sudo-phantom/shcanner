ea76de69d214b3c49ef8659934d5abc101d6b3ad
sudo-phantom <sudophantom@gmail.com>
2021-08-03 00:54:40 -0700

    Update shcanner.sh

--- shcanner.sh
+++ shcanner.sh
@@ -3,10 +3,10 @@
 ## scope.txt must be in nmap list format to work with current configuration
 
 #install needed tools
-sudo apt-get update && sudo apt-get install -y nmap python python-pip traceroute;
+sudo apt-get update && sudo apt-get install -y nmap python3 python3-pip traceroute;
 
 # run nmap scanns
-sudo nmap -sP -iL  scope.txt -oA livehost_icmp -vv;
+sudo nmap -sn -iL  scope.txt -oA livehost_icmp -vv;
 sudo nmap -sS -Pn -vv -p21,22,23,25,80,110,443,513,3389,6000,8080,8443,445,135,139 -iL scope.txt -oA livehost_standard;
 cat livehost_icmp.gnmap | grep Up | cut -d " " -f 2 > up.txt | nmap -n -Pn -iL up.txt -vv -oG full_livehosts
 #traceroute hosts
