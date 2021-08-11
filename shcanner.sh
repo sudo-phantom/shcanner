35aab62e749ffce7f2fdd10ab6ad7735721d2f46
sudo-phantom <sudophantom@gmail.com>
2021-08-11 11:24:39 -0700

    Update shcanner.sh

--- shcanner.sh
+++ shcanner.sh
@@ -7,7 +7,7 @@ sudo apt-get update && sudo apt-get install -y nmap python3 python3-pip tracerou
 
 # run nmap scanns
 sudo nmap -sn -iL  scope.txt -oA livehost_icmp -vv;
-sudo nmap -sS -Pn -vv -p21,22,23,25,80,110,443,513,3389,6000,8080,8443,2222,445,135,139 -iL scope.txt -oA livehost_standard;
+sudo nmap -sT -Pn -vv -p21,22,23,25,80,110,443,513,3389,6000,8080,8443,2222,445,135,139 -iL scope.txt -oA livehost_standard;
 cat livehost_icmp.gnmap | grep Up | cut -d " " -f 2 > up.txt | nmap -n -Pn -iL up.txt -vv -oG full_livehosts
 cat livehost_standard.gnmap | grep -e 80/open -e 443/open -e 8080/open | cut -d " " -f 2 > "webhosts.txt"
 cat livehost_standard.gnmap | grep -e 22/open -e 2222/open | cut -d " " -f 2 > "ssh-hosts.txt"
