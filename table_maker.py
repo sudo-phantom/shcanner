73ad4f8b89e5f4821742a84d410d16424c5ab5a8
sudo-phantom <sudophantom@gmail.com>
2021-09-07 16:58:50 -0700

    Update table_maker.py

--- table_maker.py
+++ table_maker.py
@@ -1,11 +1,8 @@
 import sys
-from bs4 import BeautifulSoup
 import argparse
-from numpy import dtype, inner
+from tabulate import tabulate
 import pandas as pd
-from pandas import json_normalize
-from pandas.core.frame import DataFrame
-from pandas.io.parsers import read_csv
+
 
 parser = argparse.ArgumentParser(description='convert nmap csv files to table')
 parser.add_argument('input_file', type=argparse.FileType('r'))
@@ -15,4 +12,5 @@ args = parser.parse_args()
 with args.input_file as f:
     data = pd.read_csv(f, delimiter=';', usecols=['host', 'port', 'state', 'reason', 'cpe'])
 df = pd.DataFrame(data).fillna("Not found")
-print(df.loc[df['state'] != 'closed'])
+df = df.loc[df['state'] != 'closed']
+print(tabulate(df, headers = 'keys', tablefmt = 'psq1'))
