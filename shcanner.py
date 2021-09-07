be4c4e37923cd076cb97e3796aec5b59ebfea226
sudo-phantom <sudophantom@gmail.com>
2021-09-07 13:47:41 -0700

    Update shcanner.py

--- shcanner.py
+++ shcanner.py
@@ -1,5 +1,6 @@
 import nmap
 import os
+from bs4 import BeautifulSoup
 import sys
 import itertools
 import re
@@ -48,9 +49,8 @@ def find_webhosts():
     print("finding services for: " )
     nm = nmap.PortScanner()
     raw = nm.scan('nmap -sV -iL ./py-results/up.txt -sC -vv -4 -p 80,443,8080,8443 --script ssl-enum-ciphers -oN ./py-results/webhost.nmap')
-    file = open('./py-results/livewebhosts.json', 'w')
-    file.write(json.dumps(raw))
-    file.close()
+    print(nm.csv(),  file=open('./py-results/livewebhosts.csv', 'w'))
+
                
             # Need to add parsing to compare against weak-ciphers.txt
 
@@ -63,18 +63,15 @@ def trace_hosts():
                 print("finding routes for: " + line )
                 nm = nmap.PortScanner()
                 raw = nm.scan('nmap -sn -iL ./py-results/up.txt -4 -vv 20 --traceroute -oN ./py-results/trace-hosts.nmap')
-                file = open('./py-results/trace-hosts.json', 'w')
-                file.write(json.dumps(raw))
-                file.close
+                print(nm.csv(),  file=open('./py-results/trace-hosts.csv', 'w'))
+
 
 #full scan
 temp_bar = []
 def find_full_scan(): 
         nm = nmap.PortScanner()
         raw = nm.scan('nmap -sV -sC -O -iL ./py-results/up.txt -4 -vv --top-ports 20  -oN ./py-results/full-hosts.nmap')
-        file = open('./py-results/Full-hosts.json', 'w')
-        file.write(json.dumps(raw))
-        file.close
+        print(nm.csv(),  file=open('./py-results/Full-hosts.csv', 'w'))
         print('scanning full host list for \n')
 
     
