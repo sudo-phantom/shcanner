332f489ef1296cfebf8736e82bdb16f7c5f4aef1
sudo-phantom <sudophantom@gmail.com>
2021-09-08 16:23:41 -0700

    Update shcanner.py

--- shcanner.py
+++ shcanner.py
@@ -17,29 +17,25 @@ except FileExistsError:
     pass
 
 def find_live():
-#test for live hosts
-    with open('scope.txt', 'r') as f:
-        for line in f:
-            nm = nmap.PortScanner()
-            nm.scan(hosts=line, arguments="-sn -iL scope.txt -4 -oG ./py-results/livehosts.gnmap -vv -oN ./py-results/currently-acive.nmap")
-            nm.scaninfo()
-            print("discovering: " + line + " \n")
+        nm = nmap.PortScanner()
+        nm.scan("nmap -sn -iL scope.txt -4 -oG ./py-results/livehosts.gnmap -vv -oN ./py-results/currently-acive.nmap")
+        nm.scaninfo()
+        print("discovering live hosts: \n")
 #search for hosts that are UP   
-    query = "Up"
-    temp = []
+query = "Up"
+temp = []
 
-
-    with open('./py-results/livehosts.gnmap', 'r') as grep:
-        for line in grep:
-            if query in line:
-                bar = ''.join(line).strip()
+with open('./py-results/livehosts.gnmap', 'r') as grep:
+    for line in grep:
+        if query in line:
+            bar = ''.join(line).strip()
                 #temp.append(bar.split(' ', 1))
-                bar_title, bar_ip, bar_status, bar_stat = bar.split(' ')
-                print(bar_ip + ' - ' + bar_stat)
-                temp.append(bar_ip)
-            file = open('./py-results/up.txt', 'w')
-            file.write('\n'.join(temp))
-            file.close()
+            bar_title, bar_ip, bar_status, bar_stat = bar.split(' ')
+            print(bar_ip + ' - ' + bar_stat)
+            temp.append(bar_ip)
+        file = open('./py-results/up.txt', 'w')
+        file.write('\n'.join(temp))
+        file.close()
 
 #section to find web servers 
 def find_webhosts(): 
@@ -48,7 +44,7 @@ def find_webhosts():
 
     print("finding services: " )
     nm = nmap.PortScanner()
-    raw = nm.scan('nmap -sV -iL ./py-results/up.txt -sC -vv -4 -p 80,443,8080,8443,8000,8888 --script ssl-enum-ciphers -oN ./py-results/webhost.nmap')
+    nm.scan('nmap -sV -iL ./py-results/up.txt -sC -vv -4 -p 80,443,8080,8443,8000,8888 --script ssl-enum-ciphers -oN ./py-results/webhost.nmap')
     print(nm.csv(),  file=open('./py-results/livewebhosts.csv', 'w'))
 
                
@@ -58,7 +54,7 @@ def find_webhosts():
 
 def trace_hosts():
     nm = nmap.PortScanner()
-    raw = nm.scan('nmap -sn -iL ./py-results/up.txt -4 -vv 20 --traceroute -oN ./py-results/trace-hosts.nmap')
+    nm.scan('nmap -sn -iL scope.txt -4 -vv 20 --traceroute -oN ./py-results/trace-hosts.nmap')
     print(nm.csv(),  file=open('./py-results/trace-hosts.csv', 'w'))
     print("finding routes: " )
 
@@ -67,7 +63,7 @@ def trace_hosts():
 temp_bar = []
 def find_full_scan(): 
         nm = nmap.PortScanner()
-        raw = nm.scan('nmap -sV -sC -iL ./py-results/up.txt -4 -vv --top-ports 50  -oN ./py-results/full-hosts.nmap')
+        nm.scan('nmap -sV -sC -iL ./py-results/up.txt -4 -vv --top-ports 50  -oN ./py-results/full-hosts.nmap')
         print(nm.csv(),  file=open('./py-results/Full-hosts.csv', 'w'))
         print('scanning full host list for \n')
 
