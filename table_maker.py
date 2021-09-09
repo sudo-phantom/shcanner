025ba68b511369506bb29afd8dae4d22ec6ff7d2
sudo-phantom <sudophantom@gmail.com>
2021-09-08 17:32:33 -0700

    Update table_maker.py
    added argument for open ports only

--- table_maker.py
+++ table_maker.py
@@ -6,11 +6,15 @@ import pandas as pd
 
 parser = argparse.ArgumentParser(description='convert nmap csv files to table')
 parser.add_argument('input_file', type=argparse.FileType('r'))
+parser.add_argument('--open', help='use --open to show only open ports', action="store_true" )
 args = parser.parse_args()
 
 
 with args.input_file as f:
     data = pd.read_csv(f, delimiter=';', usecols=['host', 'port', 'state', 'reason', 'cpe'])
 df = pd.DataFrame(data).fillna("Not found")
-df = df.loc[df['state'] != 'closed']
+if args.open is not None:
+    df = df.loc[df['state'] == 'open']
+else:
+    df = df.loc[df['state'] != 'closed']
 print(tabulate(df, headers = 'keys', tablefmt = 'psq1'))
