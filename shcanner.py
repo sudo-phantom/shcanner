1c312e3dcf57fc19ee09d179ae715ae0de36fe20
sudo-phantom <sudophantom@gmail.com>
2021-12-30 21:09:28 -0700

    Update shcanner.py

--- shcanner.py
+++ shcanner.py
@@ -1,7 +1,33 @@
+from typing import Iterable
 import nmap
 from concurrent.futures import ThreadPoolExecutor
 import os
 import sys
+from xml import etree
+import xml.etree.ElementTree as ET
+import argparse
+import json
+
+
+
+#add arguments for optional host declaration
+parser = argparse.ArgumentParser(description='Network segmentation automation validation tool.', fromfile_prefix_chars='@')
+parser.add_argument('-n', '--network',  help='enter an ip or network to initialize scanning.')
+parser.add_argument('-f', '--file', help='option to use file containing multiple networks, or ip address', type=argparse.FileType())
+parser.add_argument('-o','--output', default='output', help='optional ouput name to prepend to scan outputs.')
+
+args = parser.parse_args()
+
+
+if args.network is not None:
+    hosts = args.network
+elif  args.file is not None:
+    hosts = args.file.read()
+else:
+    print('host argument not set')
+    os.error
+
+output = args.output
 
 #from xml.etree import ElementTree as ET
 
@@ -16,13 +42,17 @@ query = "Up"
 temp = []
 def find_live():
         nm = nmap.PortScanner()
-        nm.scan("nmap -sn -iL scope.txt -4 -oG ./py-results/livehosts.gnmap --privileged -vv -oN ./py-results/currently-acive.nmap")
-        nm.scaninfo()
-        print("discovering live hosts: \n")
+        try:
+            nm.scan(f"-sn {hosts} -4 -oA ./py-results/{output} --privileged -vv ")
+        except:
+            ValueError
+        else:
+            nm.scaninfo()
+        print(f"discovering live hosts: {hosts} \n")
 #search for hosts that are UP   
 
 
-        with open('./py-results/livehosts.gnmap', 'r') as grep:
+        with open(f'./py-results/{output}.gnmap', 'r') as grep:
             for line in grep:
              if query in line:
                 bar = ''.join(line).strip()
@@ -37,12 +67,41 @@ def find_live():
 #section to find web servers 
 def find_webhosts(): 
     
-    print("finding services: " )
+    print("finding Web services: " )
     
     nm = nmap.PortScanner()
-    nm.scan('nmap -sV -iL ./py-results/up.txt -sC -vv -4 -p 80,443,8080,8443,8000,8888 --script ssl-enum-ciphers --privileged -oN ./py-results/webhost.nmap')
-    print(nm.csv(),  file=open('./py-results/livewebhosts.csv', 'w'))
-
+    try:
+        nm.scan(f'nmap -sV -iL ./py-results/up.txt -sC -vv -4 -p 80,443,8080,8443,8000,8888 --script ssl-enum-ciphers --privileged -oA ./py-results/{output}-webhost --host-timeout 50 --min-parallelism 4')
+    except:
+            ValueError
+    
+     
+    #print(nm.csv(),  file=open('./py-results/livewebhosts.csv', 'w'))
+def find_ciphers():
+    print('comparing ciphers:')
+    #open webhost scan results into a list
+    content_list = []
+    with open(f'./py-results/{output}-webhost.nmap') as f:
+        content_list = f.readlines()
+        #print(content_list)
+    #open weak ciphers into a list    
+    with open('./reqs/weak-ciphers') as p:
+        weak_ciphers = p.readlines()
+        print(weak_ciphers.strip())
+    #compare ciphers lists
+    found_cipher = []
+    for weak_cipher in weak_ciphers:
+        if weak_cipher in content_list:
+            print(f'detected {weak_cipher}')
+            found_cipher.append(weak_cipher.strip())
+    #print output of ciphers
+    with open(f'./py-results/{output}-compared-ciphers.txt', 'w') as file_out:
+     if weak_cipher in found_cipher:
+        print(f'Found Bad Cipher: {weak_cipher}\n')
+        file_out.write(weak_cipher)
+     else:
+        print('no weak ciphers detected.')
+        file_out.write('No weak ciphers detected.')           
                
             # Need to add parsing to compare against weak-ciphers.txt
 
@@ -50,17 +109,26 @@ def find_webhosts():
 
 def trace_hosts():
     nm = nmap.PortScanner()
-    nm.scan('nmap -sn -iL scope.txt -4 -vv 20 --traceroute --privileged -oN ./py-results/trace-hosts.nmap')
-    print(nm.csv(),  file=open('./py-results/trace-hosts.csv', 'w'))
     print("finding routes: " )
+    try:
+        nm.scan(f'-sn -iL ./py-results/up.txt -4 -vv 20 --traceroute --privileged -oA ./py-results/{output}-trace-hosts --host-timeout 25 --min-parallelism 4')
+    except:
+            ValueError
+    #print(nm.csv(),  file=open('./py-results/trace-hosts.csv', 'w'))
+    
 
 
 #full scan
 temp_bar = []
 def find_full_scan(): 
         nm = nmap.PortScanner()
-        nm.scan('nmap -sV -sC -iL ./py-results/up.txt -4 -vv --privileged --top-ports 50  -oN ./py-results/full-hosts.nmap')
-        print(nm.csv(),  file=open('./py-results/Full-hosts.csv', 'w'))
+        print("Running top 50 scan: " )
+        try:
+            nm.scan(f'nmap -sV -sC -iL ./py-results/up.txt -4 -vv --privileged --top-ports 50  -oA ./py-results/{output}-full-hosts --host-timeout 50 --min-parallelism 4')
+        except:
+            ValueError
+
+        #print(nm.csv(),  file=open(f'./py-results/{output}Full-hosts.csv', 'w'))
         print('scanning full host list for \n')
 
     
@@ -70,4 +138,5 @@ if __name__ == "__main__":
     with ThreadPoolExecutor(max_workers= 7) as executor:
         executor.map(find_webhosts())
         executor.map(trace_hosts())
+        executor.map(find_ciphers())
         executor.map(find_full_scan())
